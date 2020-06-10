e11398119db1cb8bac6bb78c798a2b398a7447e4
Amanda Alves <amanda.asilva@hotmail.com.br>
2020-06-10 07:29:02 -0300

    Projeto concluido simulando o registro em uma pensao

---
+++ Pensao/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Pensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Quarto[] vetorQuartos = new Quarto[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                vetorQuartos[i] = new Quarto();
+            }
+
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            int aux = 1;
+            while (aux <= n)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aluguel #" + aux + ":");
+                Console.Write("Nome: ");
+                string nomeInquilino = Console.ReadLine();
+                Console.Write("Email: ");
+                string emailInquilino = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int numeroQuarto = int.Parse(Console.ReadLine());
+                
+
+                vetorQuartos[numeroQuarto].NomeInquilino = nomeInquilino;
+                vetorQuartos[numeroQuarto].EmailInquilino = emailInquilino;
+                vetorQuartos[numeroQuarto].NumeroQuarto = numeroQuarto;
+
+                aux++;
+
+
+            }
+
+            Console.WriteLine("\nQuartos Ocupados: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetorQuartos[i].NumeroQuarto != 0)
+                {
+                    Console.WriteLine(i + ": " + vetorQuartos[i].NomeInquilino + ", " + vetorQuartos[i].EmailInquilino);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Pensao/Quarto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensao
+{
+    class Quarto
+    {
+        internal object nomeInquilino;
+
+        public string NomeInquilino { get; set; }
+        public string EmailInquilino { get; set; }
+        public int NumeroQuarto { get; set; }
+
+        public Quarto() { }
+
+
+        public Quarto(string nomeInquilino, string emailInquilino, int numeroQuarto)
+        {
+            NomeInquilino = nomeInquilino;
+            EmailInquilino = emailInquilino;
+            NumeroQuarto = numeroQuarto;
+        }
+
+        public override string ToString()
+        {
+            return NomeInquilino + "\n" + EmailInquilino + "\n" + NumeroQuarto;
+        }
+    }
+}
